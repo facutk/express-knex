ffc50a694d5e73766245d620975413c2682f55a5
facu.tk <facu.tk@gmail.com>
2016-07-07 15:43:53 -0300

    first commit

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+        client: 'pg',
+        connection: {
+            host: 'localhost',
+            user: 'facutk',
+            password: 'matrix',
+            database: 'todo'
+        }
+    }
+}
