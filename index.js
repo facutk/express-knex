1cf35d94d425e04cc4f7a1d10a6325a9bd6a77fa
facu.tk <facu.tk@gmail.com>
2016-07-14 17:43:14 -0300

    change api

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.json());
+
 var db = require('./db.js');
 
 app.set('port', (process.env.PORT || 3000));
@@ -8,33 +12,70 @@ app.get('/', function(req, res){
     res.send('hello pg');
 });
 
-app.get('/count', function(req, res) {
+app.post('/', function(req, res){
+    console.log(req.body);
+    res.send('ok');
+});
 
-    db('counter').where('uid', 1)
-                 .select('count')
-                 .first()
-                 .returning('count')
-                 .then(function(row) {
-                     res.send( row );
+app.get('/count/:uid', function(req, res) {
+
+    var uid = parseInt(req.params.uid);
+
+    db.select('count')
+    .from('counter')
+    .where('uid', uid)
+    .first()
+    .then(function(row) {
+        var count = 0;
+        if (row) count = row.count;
+        res.status(200)
+           .json({
+               count: count
+           });
     });
 
 });
 
 app.get('/count_inc', function(req, res) {
 
-    db('counter').where('uid', 1)
-                 .select('count')
-                 .first()
-                 .then(function(row) {
+    db.select('count')
+    .from('counter')
+    .where('uid', 1)
+    .first()
+    .then(function(row) {
 
-        db('counter').where('uid', 1)
-                     .update({count: row.count + 1})
-                     .returning('count')
-                     .then(function (row){
+        if (row) {
+            db.update({count: row.count + 1})
+            .from('counter')
+            .where('uid', 1)
+            .returning('count')
+            .then(function (row){
+                res.json( row );
+            });
+        } else {
 
-            res.send( row );
-        });
+            db.insert({count: 1})
+            .into('counter')
+            .returning('count')
+            .then(function (row){
+                res.send( row.count );
+            });
 
+        }
+
+    });
+
+});
+
+app.delete('/count/:uid', function(req, res) {
+
+    var uid = parseInt(req.params.uid);
+    db.del()
+    .from('counter')
+    .where('uid', uid)
+    .then(function(row) {
+        var count = 0;
+        res.json( count );
     });
 
 });
