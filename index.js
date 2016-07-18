9b3684ff28b6dc8a2781bc37860b4b461ec1d8ec
facu.tk <facu.tk@gmail.com>
2016-07-18 12:22:02 -0300

    added /public folder

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-//var router = express.Router();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
@@ -7,6 +6,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
+app.use(express.static('public'));
 app.set('port', (process.env.PORT || 3000));
 
 var count = require('./routes/count.js');
@@ -16,6 +16,11 @@ app.use('/count', count);
 app.get('/', function(req, res){
     res.send('hello pg');
 });
+
+app.use('*',function(req,res){
+  res.status(404).send('404');
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
