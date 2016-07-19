e9de4f713a9a8c2d80b8beeda47968e9ce9d77ef
facu.tk <facu.tk@gmail.com>
2016-07-19 18:53:30 -0300

    added click handler

--- src/client.js
+++ src/client.js
@@ -45,12 +45,18 @@ class TimeTag extends React.Component {
 }
 
 class CountItem extends React.Component {
+    static propTypes = {
+        children: React.PropTypes.any
+    };
     constructor(props, context) {
         super(props, context);
     }
+    _increment() {
+        console.log(this.props.value);
+    }
     render() {
         return (
-            <li>{this.props.children}</li>
+            <li>{this.props.value} : {this.props.count} <strong onClick={this._increment.bind(this)}>[+]</strong></li>
         );
     }
 }
@@ -62,15 +68,18 @@ class CounterList extends React.Component {
             counts: [
                 {
                     key: 9,
-                    value: 'a'
+                    value: 'a',
+                    count: '1'
                 },
                 {
                     key: 7,
-                    value: 'z'
+                    value: 'z',
+                    count: '0'
                 },
                 {
                     key: 5,
-                    value: 'w'
+                    value: 'w',
+                    count: '5'
                 }
             ]
         }
@@ -78,7 +87,7 @@ class CounterList extends React.Component {
 
     render() {
         var counts = this.state.counts.map( count =>
-            <CountItem key={count.key}>{count.value}</CountItem>
+            <CountItem key={count.key} value={count.value} count={count.count} />
         );
         return (
             <ul>
