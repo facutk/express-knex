e2eb4cf9c1c24048df980b81d283784201cca94e
facu.tk <facu.tk@gmail.com>
2016-07-19 11:58:13 -0300

    added list rendering sample

--- src/client.js
+++ src/client.js
@@ -1,37 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var TimeTag = React.createClass({
-    getInitialState: function() {
-        return {
+class NavBar extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            name: 'The',
+            password: 'Team'
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            password: '. team .'
+        });
+    }
+
+    render() {
+        return <h1>{this.state.name} {this.state.password}</h1>
+    }
+}
+
+class TimeTag extends React.Component {
+    constructor(props, context){
+        super(props, context);
+        this.state = {
             greeting: 'hello rrreactt'
         };
-    },
-    componentDidMount: function() {
+    }
+
+    componentDidMount () {
         this.serverRequest = $.get('api/date', function (result) {
             this.setState({
                 greeting: result.timestamp
             });
         }.bind(this));
-    },
+    }
 
-    render: function () {
+    render() {
         return (
             <h1>{this.state.greeting}</h1>
         );
     }
-});
-
-class NavBar extends React.Component {
-  render() {
-    return <h1>Timestamp</h1>
-  }
 }
+
+class CounterList extends React.Component {
+    constructor(props, contexts) {
+        super(props, contexts);
+        this.state = {
+            counts: [
+                {
+                    key: 9,
+                    value: 'a'
+                },
+                {
+                    key: 7,
+                    value: 'z'
+                },
+                {
+                    key: 5,
+                    value: 'w'
+                }
+            ]
+        }
+    }
+
+    render() {
+        var counts = this.state.counts.map( count =>
+            <li key={count.key}>{count.value}</li>
+        );
+        return (
+            <ul>
+                {counts}
+            </ul>
+        );
+    }
+}
+
 
 ReactDOM.render(
     <div>
         <NavBar />
         <TimeTag />
+        <CounterList />
     </div>,
     document.getElementById('root')
 );
