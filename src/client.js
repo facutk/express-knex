d4d4c897eb7b2652bb650cb3b7e557920e775e73
facu.tk <facu.tk@gmail.com>
2016-07-22 19:10:15 -0300

    fullstack-react timers mock

--- src/client.js
+++ src/client.js
@@ -183,7 +183,187 @@ class ProductList extends React.Component {
         });
         return (
             <div>
+                <h2>ProductList</h2>
                 {products}
+            </div>
+        );
+    }
+}
+
+class ToggleableTimerForm extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        if (this.props.isOpen) {
+            return (
+                <TimerForm />
+            );
+        } else {
+            return (
+                <div className='ui basic content center aligned segment'>
+                    <button className='ui basic button icon'>
+                        [+]
+                    </button>
+                </div>
+            );
+        }
+    }
+}
+
+class Timer extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        const elapsedString = this.props.elapsed;
+        return (
+            <div className='ui centered card'>
+                <div className='content'>
+                    <div className='header'>
+                        {this.props.title}
+                    </div>
+                    <div className='meta'>
+                        {this.props.project}
+                    </div>
+                    <div className='centered aligned description'>
+                        <h2>{elapsedString}</h2>
+                    </div>
+                    <div className='extra content'>
+                        <span className='right floated edit icon'>[edit]</span>
+                        <span className='right floated trash icon'>[delete]</span>
+                    </div>
+                </div>
+                <div className='ui bottom attached blue basic button'>
+                    Start
+                </div>
+            </div>
+        );
+    }
+}
+
+class TimerForm extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        const submitText = this.props.title ? 'Update' : 'Create';
+        return (
+            <div className='ui centered card'>
+                <div className='content'>
+                    <div className='ui form'>
+                        <div className='field'>
+                            <label>Title</label>
+                            <input type='text' defaultValue={this.props.title} />
+                        </div>
+                        <div className='field'>
+                            <label>Project</label>
+                            <input type='text' defaultValue={this.props.Project} />
+                        </div>
+                        <div className='ui two bottom attached buttons'>
+                            <button className='ui basic blue button' type='text'>
+                                {submitText}
+                            </button>
+                            <button className='ui basic red button' type='text'>
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class EditableTimer extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        if (this.props.editFormOpen) {
+            return (
+                <TimerForm
+                    title={this.props.title}
+                    project={this.props.project}
+                />
+            );
+        } else {
+            return (
+                <Timer
+                    title={this.props.title}
+                    project={this.props.project}
+                    elapsed={this.props.elapsed}
+                    runningSince={this.props.runningSince}
+                />
+            );
+        }
+    }
+}
+
+class EditableTimerList extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        const timers = this.props.timers.map((timer) => {
+            return (
+                <EditableTimer
+                    key={timer.id}
+                    id={timer.id}
+                    title={timer.title}
+                    project={timer.project}
+                    elapsed={timer.elapsed}
+                    runningSince={timer.runningSince}
+                />
+            );
+        });
+        return (
+            <div id='timers'>
+                {timers}
+            </div>
+        );
+    }
+}
+
+class TimersDashboard extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            timers: [
+                {
+                    title: 'Practice squat',
+                    project: 'Gym Chores',
+                    id: 1,
+                    elapsed: 5456099,
+                    runningSince: Date.now()
+                },
+                {
+                    title: 'Bake squash',
+                    project: 'Kitchen Chores',
+                    id: 2,
+                    elapsed: 1273998,
+                    runningSince: null
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='column'>
+                    <EditableTimerList
+                        timers={this.state.timers}
+                    />
+                    <ToggleableTimerForm
+                        isOpen={true}
+                    />
+                </div>
             </div>
         );
     }
@@ -192,9 +372,15 @@ class ProductList extends React.Component {
 ReactDOM.render(
     <div>
         <NavBar />
+        <hr />
         <TimeTag />
+        <hr />
         <CounterList />
+        <hr />
         <ProductList />
+        <hr />
+        <h2>timers</h2>
+        <TimersDashboard />
     </div>,
     document.getElementById('root')
 );
